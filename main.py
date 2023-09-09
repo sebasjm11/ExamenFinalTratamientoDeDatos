75c07008a1c46525ddda81ebecbc236cf840ffc8
sebasjm11 <cbasjara11@hotmail.com>
2023-09-09 12:41:18 -0500

    Final2

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from db import MongoDriver
 
+consulta = 'Real Madrid Retro'
 
 driver = webdriver.Chrome()
 driver.get("https://www.camisetasfutboleses.com/")
 search_box = driver.find_element(by=By.CSS_SELECTOR, value="#search > input")
-search_box.send_keys("Real Madrid Retro")
+search_box.send_keys(consulta)
 
 search_button = driver.find_element(by=By.CSS_SELECTOR, value="#search > span > button > span > i")
 search_button.click()
@@ -24,23 +25,29 @@ mongodb = MongoDriver()
 for card in camiseta:
     try:
         nombre = card.find_element(By.CSS_SELECTOR, "div > div > div.product-details > div.caption > h4").text
-        precio = card.find_element(By.CSS_SELECTOR, "div > div > div.product-details > div.caption > p.price > span.price-new").text
+        precioof = card.find_element(By.CSS_SELECTOR, "div > div > div.product-details > div.caption > p.price > span.price-new").text
         descuento = card.find_element(By.CSS_SELECTOR, "div > div > div.product-details > div.caption > p.price > span.percentsaving").text
+        precion = card.find_element(By.CSS_SELECTOR,"div > div > div.product-details > div.caption > p.price > span.price-old").text
         # Tu código para procesar otros elementos aquí
 
         print(nombre)
-        print(precio)
+        print(precioof)
         print(descuento)
+        print(precion)
         # Imprime otros datos aquí
 
         Camiseta = {
             "Nombre": nombre,
-            "Precio": precio,
-            "Descuento": descuento,
-            # Agrega otros datos aquí
+            "Resultado": {
+                "Precio Oferta": precioof,
+                "Descuento": descuento,
+                "Precio Normal": precion,
+                # Agrega otros datos aquí
+            }
+
         }
 
-        mongodb.insert_record(record=Camiseta, username="Camiseta Real Madrid")
+        mongodb.insert_record(record=Camiseta, username="Camisetas Real Madrid")
 
         print("++++++++++++++++++++++++++++++++")
     except Exception as e:
